bcda52bc27675c682ab4a1216ec0820f476863df
nito nise <aakazakovv@yandex.ru>
2020-12-25 16:22:13 +0300

    add user entity

---
+++ backend/src/main/java/dev/fun/store/backend/domain/User.java
@@ -0,0 +1,46 @@
+package dev.fun.store.backend.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+	private static final String SEQUENCE_NAME = "user_seq";
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENCE_NAME)
+	@SequenceGenerator(name = SEQUENCE_NAME, sequenceName = SEQUENCE_NAME, allocationSize = 1)
+	@Column(name = "id")
+	@Setter(value = AccessLevel.PRIVATE)
+	private long id;
+	
+	@Column(name = "login", nullable = false)
+	private String login;
+	
+	@Column(name = "password", nullable = false)
+	private String password;
+	
+	@Column(name = "enabled", nullable = false)
+	private Boolean enabled;
+
+	public User(String login, String password, Boolean enabled) {
+		this.login = login;
+		this.password = password;
+		this.enabled = enabled;
+	}
+	
+}
