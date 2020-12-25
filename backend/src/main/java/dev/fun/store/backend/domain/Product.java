9a23a3a4ac39f2ef7f8e93454f6ac49334dc0d90
nito nise <aakazakovv@yandex.ru>
2020-12-25 16:05:24 +0300

    add product entity

---
+++ backend/src/main/java/dev/fun/store/backend/domain/Product.java
@@ -0,0 +1,49 @@
+package dev.fun.store.backend.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import dev.fun.store.backend.finance.Money;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "products")
+public class Product {
+	
+	private static final String SEQUENCE_NAME = "product_seq";
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENCE_NAME)
+	@SequenceGenerator(name = SEQUENCE_NAME, sequenceName = SEQUENCE_NAME, allocationSize = 1)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "title", nullable = false)
+	private String title;
+	
+	@Column(name = "cost", nullable = false)
+	private Long cost;
+
+	public Product(String title, Long cost) {
+		this.title = title;
+		this.cost = cost;
+	}
+	
+	public Product(String title, Double cost) {
+		this.title = title;
+		this.cost = Money.doubleToLong(cost);
+	}
+	
+	public double getCostInDoubleValue() {
+		return Money.longToDouble(cost);
+	}
+	
+}
