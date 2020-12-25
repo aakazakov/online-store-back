1fd54fc1de2cd7ef3193f10a3a1bd1fd029c5852
nito nise <aakazakovv@yandex.ru>
2020-12-25 19:07:59 +0300

    fix javadoc

--- backend/src/main/java/dev/fun/store/backend/finance/Money.java
+++ backend/src/main/java/dev/fun/store/backend/finance/Money.java
@@ -3,7 +3,10 @@ package dev.fun.store.backend.finance;
 public class Money {
 		
 	/**
-	 * Converts the currency value {@code double} to its equivalent {@code long}.
+	 * Converts the currency {@code double} value  to its {@code long} equivalent.<p>
+	 * For example:<p>
+	 * <code>doubleToLong(98.95)</code> will return <code>9895L</code><p>
+	 * 98.95 USD = 9895 cents
 	 * @param value {@code double} number
 	 * @return {@code long} number
 	 */
@@ -12,7 +15,10 @@ public class Money {
 	}
 	
 	/**
-	 * Converts the currency value {@code long} to its equivalent {@code double}.
+	 * Converts the currency {@code long} value  to its {@code double} equivalent.
+	 * For example:<p>
+	 * <code>longToDouble(9895L)</code> will return <code>98.95</code><p>
+	 * 9895 cents = 98.95 USD
 	 * @param value {@code long} number
 	 * @return {@code double} number
 	 */
