5ec2352e0495235be57ba1fa8f497680b33813ce
nito nise <aakazakovv@yandex.ru>
2021-01-12 20:37:27 +0300

    add basket controller

---
+++ backend/src/main/java/dev/fun/store/backend/controller/BasketController.java
@@ -0,0 +1,38 @@
+package dev.fun.store.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.fun.store.backend.dto.InputBasketDto;
+import dev.fun.store.backend.dto.OutputBasketDto;
+import dev.fun.store.backend.service.BasketService;
+import dev.fun.store.backend.service.BasketServiceImpl;
+
+@RestController
+@RequestMapping(path = "/baskets", produces = MediaType.APPLICATION_JSON_VALUE)
+public class BasketController {
+	
+	private final BasketService basketService;
+	
+	@Autowired	
+	public BasketController(BasketServiceImpl basketService) {
+		this.basketService = basketService;
+	}
+
+	@PostMapping(path = "/create", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public OutputBasketDto createBasket(@RequestBody InputBasketDto dto) {
+		return basketService.createBasket(dto);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public void deleteBasket(@PathVariable(name = "id") Long id) {
+		basketService.deleteBasket(id);
+	}
+	
+}
