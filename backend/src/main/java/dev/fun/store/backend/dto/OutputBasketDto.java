910bb819b3546fbddd90f970fc519af3f2b513b5
nito nise <aakazakovv@yandex.ru>
2021-01-12 16:47:09 +0300

    add ID field

--- backend/src/main/java/dev/fun/store/backend/dto/OutputBasketDto.java
+++ backend/src/main/java/dev/fun/store/backend/dto/OutputBasketDto.java
@@ -7,6 +7,7 @@ import lombok.Data;
 @Data
 public class OutputBasketDto {
 
+	private Long basketId;
 	private List<BasketDetailDto> details;
 	private Integer totalAmount;
 	private Long totalCost;
