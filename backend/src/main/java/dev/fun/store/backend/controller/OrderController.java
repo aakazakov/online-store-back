59dc0f87da8166a883377e6e87da4556b753ee9c
nito nise <aakazakovv@yandex.ru>
2021-01-22 16:58:25 +0300

    add order conroller

---
+++ backend/src/main/java/dev/fun/store/backend/controller/OrderController.java
@@ -0,0 +1,36 @@
+package dev.fun.store.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.fun.store.backend.dto.OrderDto;
+import dev.fun.store.backend.service.OrderService;
+
+@RestController
+@RequestMapping(path = "/orders", produces = MediaType.APPLICATION_JSON_VALUE)
+public class OrderController {
+	
+	private final OrderService orderService;
+	
+	@Autowired
+	public OrderController(OrderService orderService) {
+		this.orderService = orderService;
+	}
+
+	@PostMapping(path = "/create", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public OrderDto createOrder(@RequestBody OrderDto dto) {
+		return orderService.createNewOrder(dto);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public void deleteOrder(@PathVariable(name = "id") Long id) {
+		orderService.deleteOrder(id);
+	}
+	
+}
---
+++ backend/src/test/java/dev/fun/store/backend/controller/OrderControllerTest.java
@@ -0,0 +1,81 @@
+package dev.fun.store.backend.controller;
+
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import dev.fun.store.backend.dto.OrderDto;
+import dev.fun.store.backend.service.OrderServiceImpl;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(OrderController.class)
+class OrderControllerTest {
+	
+	@Autowired
+	MockMvc mockMvc;
+	
+  @Autowired
+  ObjectMapper objectMapper;
+  
+  @MockBean
+  OrderServiceImpl orderService;
+  
+  List<OrderDto> orderDtoList = new ArrayList<>();
+  
+  @BeforeEach
+  void setup() {
+  	OrderDto dto1 = new OrderDto(); dto1.setId(1L);
+  	OrderDto dto2 = new OrderDto(); dto2.setId(2L);
+  	OrderDto dto3 = new OrderDto(); dto3.setId(3L);
+  	
+  	orderDtoList.addAll(Arrays.asList(dto1, dto2, dto3));
+  }
+
+	@Test
+	void testCreateOrder() throws JsonProcessingException, Exception {
+		OrderDto dto = orderDtoList.get(0);
+		
+		Mockito.when(orderService.createNewOrder(Mockito.any(OrderDto.class))).thenReturn(dto);
+		
+		mockMvc
+			.perform(MockMvcRequestBuilders.post("/orders/create")
+					.contentType(MediaType.APPLICATION_JSON_VALUE)
+					.content(objectMapper.writeValueAsString(dto))
+					.characterEncoding("utf-8"))
+			.andExpect(status().isOk())
+		  .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(1)));
+	}
+	
+	@Test
+	void testDeleteOrder() throws Exception {
+		Long id = 1L;
+		
+		mockMvc
+			.perform(MockMvcRequestBuilders.delete("/orders/delete/{id}", id)
+					.accept(MediaType.APPLICATION_JSON_VALUE))
+			.andExpect(status().isOk());
+		
+		verify(orderService).deleteOrder(id);
+	}
+
+}
